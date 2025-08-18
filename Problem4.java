488f65009dc8efd9bc443e29d98095b23a3ee8b0
Snehalkale24 <snehalbkale2004@gmail.com>
2025-08-18 21:46:25 +0530

    add promblem4

---
+++ Problem4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Problem4 {
+    public static void merge(int[] arr1, int[] arr2) {
+        int m = arr1.length, n = arr2.length;
+        int i = m - 1, j = 0;
+
+        while (i >= 0 && j < n) {
+            if (arr1[i] > arr2[j]) {
+                int temp = arr1[i];
+                arr1[i] = arr2[j];
+                arr2[j] = temp;
+            }
+            i--;
+            j++;
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+
+        merge(arr1, arr2);
+
+        System.out.println("arr1: " + Arrays.toString(arr1));
+        System.out.println("arr2: " + Arrays.toString(arr2));
+    }
+}
